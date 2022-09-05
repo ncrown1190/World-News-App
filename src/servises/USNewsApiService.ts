f7acc03924e6302c8e77847beb85bfd347a21a55
ncrown1190 <ncrown88@outlook.com>
2022-09-05 09:19:26 -0400

    env local added

--- src/servises/USNewsApiService.ts
+++ src/servises/USNewsApiService.ts
@@ -1,24 +1,33 @@
 import axios from "axios";
 
+const apiKey = process.env.REACT_APP_NEWS_API_KEY || "";
+
 export function fetchUSNews() {
   return axios
-    .get(
-      "https://newsapi.org/v2/top-headlines?country=us&apiKey=6004b8fcb1604003b4ead57854e8d2c2"
-    )
+    .get("https://newsapi.org/v2/top-headlines?country=us", {
+      params: { apiKey: apiKey },
+    })
     .then((response) => response.data);
 }
+// export function fetchUSNews() {
+//   return axios
+//     .get(
+//       "https://newsapi.org/v2/top-headlines?country=us&apiKey=6004b8fcb1604003b4ead57854e8d2c2"
+//     )
+//     .then((response) => response.data);
+// }
 
 export function fetchINDNews() {
   return axios
-    .get(
-      "https://newsapi.org/v2/top-headlines?country=in&apiKey=6004b8fcb1604003b4ead57854e8d2c2"
-    )
+    .get("https://newsapi.org/v2/top-headlines?country=in", {
+      params: { apiKey: apiKey },
+    })
     .then((response) => response.data);
 }
 export function getNewsByPub() {
   return axios
-    .get(
-      "https://newsapi.org/v2/top-headlines/sources?apiKey=6004b8fcb1604003b4ead57854e8d2c2"
-    )
+    .get("https://newsapi.org/v2/top-headlines/sources", {
+      params: { apiKey: apiKey },
+    })
     .then((response) => response.data);
 }
